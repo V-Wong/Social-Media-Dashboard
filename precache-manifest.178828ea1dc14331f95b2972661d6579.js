ae9d075f508f5607208673d5942383fc3f5a96b6
Vincent Wong <TheBrutux168@gmail.com>
2020-04-24 22:19:49 +1000

    Updates

---
+++ precache-manifest.178828ea1dc14331f95b2972661d6579.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a9d755d0e3ea9996af129020891214",
+    "url": "/Social-Media-Dashboard/index.html"
+  },
+  {
+    "revision": "a0d7c31cc8f9fc9975c5",
+    "url": "/Social-Media-Dashboard/static/css/main.e1adcc93.chunk.css"
+  },
+  {
+    "revision": "aafe0d482ee9c363f531",
+    "url": "/Social-Media-Dashboard/static/js/2.86d9eea7.chunk.js"
+  },
+  {
+    "revision": "80af19f49bad859724b293d8aab86865",
+    "url": "/Social-Media-Dashboard/static/js/2.86d9eea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d7c31cc8f9fc9975c5",
+    "url": "/Social-Media-Dashboard/static/js/main.f4c426eb.chunk.js"
+  },
+  {
+    "revision": "5c044877fcbf8a69ae1e",
+    "url": "/Social-Media-Dashboard/static/js/runtime-main.dd1c2196.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.80e16074a9660eddb25b0542bf017e98.js"
+  "/Social-Media-Dashboard/precache-manifest.178828ea1dc14331f95b2972661d6579.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Social-Media-Dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
